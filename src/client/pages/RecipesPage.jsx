0042b75c0112d45d467d1407f549047b6ea0c9a9
Christie <christie@Christies-MacBook-Air.local>
2024-04-28 19:37:32 -0400

    fixed routing so that the pantry page does not go abck to the pantry pade, btut only the welcome page and recipes page  and the recipe page does not go back to the recipe page but the welcome page and the pantry page, also created a welcome page that goes to the pantry page and recipe page, fixed styling issues such as button color and the border and margin

--- src/client/pages/RecipesPage.jsx
+++ src/client/pages/RecipesPage.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import RecipeCard from "../components/RecipeCard";
-
+import { Link } from 'react-router-dom';
+import Recommd from "../components/Recommd";
+import '../red-lip.css';
 
 function RecipesPage() {
   return (
@@ -8,6 +10,15 @@ function RecipesPage() {
     <div className="recipes">
       <h1>This is the Recipes page</h1>
       <RecipeCard/>
+      <Recommd/>
+      <div className="navigation-buttons">
+        <Link to="/">
+          <button type="button">Take me to the Welcome Page</button>
+        </Link>
+        <Link to="/pantry">
+          <button type="button">Take me to the Recipes Page</button>
+        </Link>
+      </div>
     </div> 
   )
 }
--- src/client/pages/WelcomePage.jsx
+++ src/client/pages/WelcomePage.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Recommd from '../components/Recommd';
+import '../red-lip.css'; 
 
 const WelcomePage = () => {
   // Function to handle alert, not fully implemented here
@@ -9,8 +11,15 @@ const WelcomePage = () => {
     <div>
       <h1>Welcome to the grocery tracker</h1>
       <button onClick={showAlert}>Show Alert</button>
-      {/* <Link to="/pantry">Add to Pantry</Link>
-      <Link to="/recipes">Recipes</Link> */}
+      <Recommd/>
+      <div className="navigation-buttons">
+        <Link to="/pantry">
+          <button type="button">Take me to the Pantry Page</button>
+        </Link>
+        <Link to="/recipes">
+          <button type="button">Take me to the Recipes Page</button>
+        </Link>
+      </div>
     </div>
   );
 };
