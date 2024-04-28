319be474d3ca5abd8edb056cb8e889309229d936
Peter Gao <peter_gao@outlook.com>
2024-04-27 22:56:18 -0700

    ajust the Dropdownlist 2

--- src/client/containers/RecipeMainContainer.js
+++ src/client/containers/RecipeMainContainer.js
@@ -2,7 +2,7 @@
  * @Author: Peter Gao 
  * @Date: 2024-04-27 16:03:29 
  * @Last Modified by: peter_gao@outlook.com
- * @Last Modified time: 2024-04-27 16:06:36
+ * @Last Modified time: 2024-04-27 22:54:47
  */
 
 
@@ -13,9 +13,15 @@ import React from 'react'
 
 export const RecipeMainContainer = (props) => {
     return (
-        <div>
+        <div style={{ margin: '40px' }}>
+            <div style={{ backgroundColor: '#FFCB77' }}>Yellow</div>
+            <div style={{ backgroundColor: '#17C3B2' }}>green</div>
+            <div style={{ backgroundColor: '#227C9D' }}>blue</div>
+            <div style={{ backgroundColor: '#FEF9EF' }}>khaki</div>
+            <div style={{ backgroundColor: '#FE6D73' }}>red</div>
             <DropdownList {...props} />
             <RecipeCard {...props} />
         </div>
     )
 }
+
