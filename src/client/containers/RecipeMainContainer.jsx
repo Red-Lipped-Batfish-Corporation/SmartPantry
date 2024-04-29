11c6f3dd623e6a09c9c8c1d41db83d3d50864aa4
Peter Gao <peter_gao@outlook.com>
2024-04-29 15:58:31 -0700

    new version

--- src/client/containers/RecipeMainContainer.jsx
+++ src/client/containers/RecipeMainContainer.jsx
@@ -2,7 +2,7 @@
  * @Author: Peter Gao 
  * @Date: 2024-04-27 16:03:29 
  * @Last Modified by: peter_gao@outlook.com
- * @Last Modified time: 2024-04-29 15:54:22
+ * @Last Modified time: 2024-04-29 15:57:16
  */
 
 
@@ -21,7 +21,7 @@ const RecipeMainContainer = (props) => {
                     <button type="button">Take me to the Welcome Page</button>
                 </Link>
                 <Link to="/pantry">
-                    <button type="button">Take me to the Recipes Page</button>
+                    <button type="button">Take me to the Pantry Page</button>
                 </Link>
             </div>
         </div>
